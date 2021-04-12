d94c5946ce8848a6a904849704b25c87561b2f64
Matthew <elijah2895@hotmail.com>
2021-04-12 11:16:33 -0400

    Done during pairing session

--- src/index.js
+++ src/index.js
@@ -13,3 +13,81 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const toyBox = document.querySelector('#toy-collection')
+
+function renderOneToy (toyObj) {
+  const outerDiv = document.createElement('div')
+  outerDiv.classList.add ('card')
+  outerDiv.dataset.id = toyObj.id
+
+  outerDiv.innerHTML = `
+  <h2>${toyObj.name}</h2>
+  <img src='${toyObj.image}' class="toy-avatar" />
+  <p> ${toyObj.likes} </p>
+  <button class="like-btn">Like <3</button>`
+  
+  toyBox.append(outerDiv)
+}
+
+function renderAllToys () {
+  fetch ('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(toyArr => {
+      toyArr.forEach(toyObject => {
+        renderOneToy(toyObject)
+      })
+    })
+}
+
+renderAllToys()
+
+const form = document.querySelector('.add-toy-form')
+
+form.addEventListener('submit', (event) => {
+  
+  event.preventDefault()
+  
+  const name = event.target.name.value
+  const image = event.target.image.value
+  // const likes = event.target.likes.value
+
+  const newToyObj = {name, image, likes: 0}
+  
+  fetch ('http://localhost:3000/toys',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(newToyObj)
+  })
+      .then(response => response.json())
+      .then(newToyObject => {
+        renderOneToy(newToyObject)
+      })
+
+      form.reset()
+})
+
+toyBox.addEventListener('click', (event) => {
+  if (event.target.className === 'like-btn') {
+    const cardDiv = event.target.closest('div.card')
+    const likesPTag = cardDiv.querySelector('p')
+    const currLikes = parseInt(likesPTag.textContent)
+    likesPTag.textContent = `${currLikes + 1}`
+
+    fetch (`http://localhost:3000/toys/${cardDiv.dataset.id}`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({ likes: currLikes + 1})
+  })
+      .then(response => response.json())
+      .then(data => {
+        likesPTag.textContent = `${data.likes}`
+      })
+  }
+})
